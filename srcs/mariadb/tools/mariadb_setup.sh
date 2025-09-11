0914cfcce5c7eee0b06350c8008968edb996c087
pnaessen <pnaessen@student.42lyon.fr>
2025-09-11 06:06:52 -0400

    Fix : sript shell secu for root and user in db can connect with pwd

--- srcs/mariadb/tools/mariadb_setup.sh
+++ srcs/mariadb/tools/mariadb_setup.sh
@@ -1,29 +1,48 @@
 #!/bin/sh
 
-if [ ! -d "/var/lib/mysql/mysql" ]; then
-    mysql_install_db --user=mysql --datadir=/var/lib/mysql --rpm
+if [ -z "$MYSQL_USER" ] || [ -z "$MYSQL_USER_PWD" ] || [ -z "$MYSQL_DATABASE" ]; then
+    echo "Error .env need (MYSQL_USER, MYSQL_PASSWORD, MYSQL_DATABASE)"
+    exit 1
 fi
 
-# mkdir -p /run/mysqld
-# chown mysql:mysql /run/mysqld
+mkdir -p /run/mysqld
+chown mysql:mysql /run/mysqld
+chown mysql:mysql /var/lib/mysql
 
-mysqld_safe --user=mysql &
+if [ ! -d "/var/lib/mysql/mysql" ]; then
+    mysql_install_db --user=mysql --datadir=/var/lib/mysql --basedir=/usr
+fi
 
-sleep 5
+mariadbd-safe --user=mysql --datadir=/var/lib/mysql &
+MARIADB_PID=$!
 
-mariadb -u root -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
-mariadb -u root -e "ALTER USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
-mariadb -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_PASSWORD';"
-mariadb -u root -p"$MYSQL_PASSWORD" -e "FLUSH PRIVILEGES;"
-# mysql -u root -e << EOF
-# CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;
-# CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY PASSWORD '$MYSQL_PASSWORD';
-# ALTER USER '$MYSQL_USER'@'%' IDENTIFIED BY PASSWORD '$MYSQL_PASSWORD';
-# GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';
-# FLUSH PRIVILEGES;
-# EOF
+for i in {1..30}; do
+    if mariadb -u root -e "SELECT 1;" > /dev/null 2>&1; then
+        break
+    fi
+    sleep 1
+done
 
-mysqladmin -u root -p"$MYSQL_PASSWORD" shutdown
+mariadb -u root <<EOF
+CREATE USER IF NOT EXISTS '${MYSQL_ROOT_USER}'@'%' IDENTIFIED BY '${MYSQL_ROOT_PWD}';
+GRANT ALL PRIVILEGES ON *.* TO '${MYSQL_ROOT_USER}'@'%' WITH GRANT OPTION;
 
-exec mysqld_safe 
-# --user=mysql
+ALTER USER 'root'@'localhost' IDENTIFIED VIA mysql_native_password USING PASSWORD('${MYSQL_ROOT_PWD}');
+ALTER USER '${MYSQL_ROOT_USER}'@'%' IDENTIFIED BY '${MYSQL_ROOT_PWD}';
+
+
+CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;
+CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_USER_PWD}';
+GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'%';
+
+DELETE FROM mysql.user WHERE User='';
+DROP DATABASE IF EXISTS test;
+DELETE FROM mysql.db WHERE Db='test' OR Db LIKE 'test_%';
+
+FLUSH PRIVILEGES;
+EOF
+
+mariadb-admin -u root -p"$MYSQL_USER_PWD" shutdown
+wait $MARIADB_PID
+
+exec mariadbd-safe --user=mysql --datadir=/var/lib/mysql
