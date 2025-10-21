ea22ad7470c36c2ca7032073bf61794495b7bd34
pnaessen <pnaessen@student.42lyon.fr>
2025-10-21 12:01:11 +0200

    Fix workflow

---
+++ srcs/requirements/bonus/ftpd/tools/entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+set -e
+
+HOST_IP=$(ip route | awk '/default/ { print $3 }')
+echo "pasv_address=${HOST_IP}" >> /etc/vsftpd/vsftpd.conf
+
+mkdir -p /home/$FTP_USER/ftp
+mkdir -p /var/www/html
+
+if ! id "$FTP_USER" &>/dev/null; then
+    adduser -D -G www-data $FTP_USER
+fi
+
+echo "$FTP_USER:$FTP_PWD" | chpasswd
+
+chown -R "$FTP_USER:www-data" /home/$FTP_USER
+chmod -R 775 /var/www/html
+
+exec /usr/sbin/vsftpd /etc/vsftpd/vsftpd.conf
